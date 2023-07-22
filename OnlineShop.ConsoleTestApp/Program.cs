5350dcbccd83dc27a7175958e0e9f034bd95b217
Азат <freedoom9654@gmail.com>
2023-07-22 13:39:03 +0300

    [OnlineShop.ConsoleTestApp] 1) test app prepared

---
+++ OnlineShop.ConsoleTestApp/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+Console.WriteLine("Hello, World!");
+
+var builder = new HostBuilder()
+    .ConfigureServices((hostContaxt, services) =>
+    {
+        var configurationBuilder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false)
+        .Build();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.AddConsole();
+        logging.SetMinimumLevel(LogLevel.Information);
+    })
+    .UseConsoleLifetime();
+
+var host = builder.Build();
+
+using(var serviceScope = host.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+
+    try
+    {
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error occured: {ex.Message}");
+    }
+}
