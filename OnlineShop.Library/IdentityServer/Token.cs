974a13768d3a5fc2e6d4b0996c02f5d0d67831a7
Azat <nam@ntc-platforma.ru>
2023-08-23 15:05:07 +0300

    null operator added

--- OnlineShop.Library/IdentityServer/Token.cs
+++ OnlineShop.Library/IdentityServer/Token.cs
@@ -6,7 +6,7 @@ namespace OnlineShop.Library.IdentityServer
     public class Token
     {
         [JsonProperty("access_token")]
-        public string AccessToken { get; set; }
+        public string? AccessToken { get; set; }
 
         [JsonProperty("expires_in")]
         public string ExpiresIn { get; set; }
